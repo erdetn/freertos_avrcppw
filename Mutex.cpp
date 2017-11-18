c9e81382a5bcb9734a8ec7d74bae5e31fa77b0ba
Erdet N <2962934+erdetn@users.noreply.github.com>
2017-11-18 12:19:31 +0100

    Add files via upload

---
+++ Mutex.cpp
@@ -0,0 +1,58 @@
+#include "Mutex.h"
+
+Mutex::Mutex()
+{
+  m_blockTime = 0;
+  m_mutex = xSemaphoreCreateMutex();
+
+  if (m_mutex != NULL)
+    m_isCreated = true;
+}
+
+Mutex::Mutex(TickType_t blockTime)
+{
+  m_blockTime = blockTime;
+  m_mutex = xSemaphoreCreateMutex();
+
+  if (m_mutex != NULL)
+    m_isCreated = true;
+}
+
+TickType_t Mutex::getBlockTime() const
+{
+  return m_blockTime;
+}
+
+bool Mutex::lock()
+{
+  return (xSemaphoreTake(m_mutex, m_blockTime) == pdTRUE);
+}
+
+bool Mutex::unlock()
+{
+  return (xSemaphoreGive(m_mutex) == pdTRUE);
+}
+
+bool Mutex::isCreated() const
+{
+  return m_isCreated;
+}
+
+bool Mutex::unlockFromISR(bool *isTaskUnblocked)
+{
+  portBASE_TYPE isTaskUnblocked_;
+  bool return_;
+
+  return_ = xSemaphoreGiveFromISR(m_mutex, &isTaskUnblocked_);
+
+  if(isTaskUnblocked_ == pdTRUE)
+  {
+    *(isTaskUnblocked) = true;
+  }
+  else
+  {
+    *(isTaskUnblocked) = false;
+  }
+
+  return (return_ == pdTRUE);
+}
---
+++ Mutex.h
@@ -0,0 +1,21 @@
+#include <Arduino_FreeRTOS.h>
+#include <semphr.h>
+
+class Mutex
+{
+  private:
+    SemaphoreHandle_t m_mutex      = NULL;
+    TickType_t        m_blockTime  = 0;
+    bool              m_isCreated  = false;
+
+  public:
+    Mutex();
+    Mutex(TickType_t blockTime);
+
+    TickType_t getBlockTime() const;
+
+    bool lock();
+    bool unlock();
+    bool unlockFromISR(bool *isTaskUnblocked);
+    bool isCreated() const;
+};
