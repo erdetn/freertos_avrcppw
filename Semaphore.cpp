947680abe434556aa5ee4a4ee1970ac2b7998903
Erdet N <2962934+erdetn@users.noreply.github.com>
2017-11-18 11:46:45 +0100

    Add files via upload

---
+++ Semaphore.cpp
@@ -0,0 +1,40 @@
+#include "Semaphore.h"
+
+Semaphore::Semaphore()
+{
+  m_blockTime = 0;
+  vSemaphoreCreateBinary(m_semaphore);
+
+  if (m_semaphore != NULL)
+    m_isCreated = true;
+}
+
+Semaphore::Semaphore(TickType_t blockTime)
+{
+  m_blockTime = blockTime;
+  vSemaphoreCreateBinary(m_semaphore);
+
+  if (m_semaphore != NULL)
+    m_isCreated = true;
+}
+
+TickType_t Semaphore::getBlockTime() const
+{
+  return m_blockTime;
+}
+
+bool Semaphore::take()
+{
+  return (xSemaphoreTake(m_semaphore, m_blockTime) == pdTRUE);
+}
+
+bool Semaphore::give()
+{
+  return (xSemaphoreGive(m_semaphore) == pdTRUE);
+}
+
+bool Semaphore::isCreated() const
+{
+  return m_isCreated;
+}
+
---
+++ Semaphore.h
@@ -0,0 +1,20 @@
+#include <Arduino_FreeRTOS.h>
+#include <semphr.h>
+
+class Semaphore
+{
+  private:
+    SemaphoreHandle_t m_semaphore  = NULL;
+    TickType_t        m_blockTime  = 0;
+    bool              m_isCreated  = false;
+
+  public:
+    Semaphore();
+    Semaphore(TickType_t blockTime);
+
+    TickType_t getBlockTime() const;
+
+    bool take();
+    bool give();
+    bool isCreated() const;
+};
