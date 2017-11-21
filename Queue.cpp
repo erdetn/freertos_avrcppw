c9bb5e018ed69e71a8cead11c41e427f07815765
Erdet N <2962934+erdetn@users.noreply.github.com>
2017-11-21 13:22:00 +0100

    Add files via upload

---
+++ Queue.cpp
@@ -0,0 +1,81 @@
+#include "Queue.h"
+
+Queue::Queue(UBaseType_t length, UBaseType_t unitSize)
+{
+    m_waitToReceive = DEFAULT_TICKS_TO_WAIT;
+    m_waitToSend    = DEFAULT_TICKS_TO_WAIT;
+    m_length        = length;
+    m_unitSize      = unitSize;
+
+    m_hQueue = xQueueCreate(m_length, m_unitSize);
+}
+
+Queue::Queue(UBaseType_t length, UBaseType_t unitSize, TickType_t defaultSendReceiveTime)
+{
+    m_waitToReceive = defaultSendReceiveTime/portTICK_PERIOD_MS;
+    m_waitToSend    = defaultSendReceiveTime/portTICK_PERIOD_MS;
+    m_length        = length;
+    m_unitSize      = unitSize;
+
+    m_hQueue = xQueueCreate(m_length, m_unitSize);
+}
+
+Queue::Queue(UBaseType_t length, UBaseType_t unitSize, TickType_t waitToSend, TickType_t waitToReceive)
+{
+    m_waitToReceive = waitToReceive/portTICK_PERIOD_MS;
+    m_waitToSend    = waitToSend/portTICK_PERIOD_MS;
+    m_length        = length;
+    m_unitSize      = unitSize;
+
+    m_hQueue = xQueueCreate(m_length, m_unitSize);
+}
+
+bool Queue::send(const void *data)
+{
+    return (xQueueSend(m_hQueue, data, m_waitToSend) == pdTRUE);
+}
+
+bool Queue::send(const void *data, TickType_t waitToSend)
+{
+    return (xQueueSend(m_hQueue, data, waitToSend) == pdTRUE);
+}
+
+bool Queue::receive(void *data)
+{
+    return (xQueueReceive(m_hQueue, data, m_waitToReceive) == pdTRUE);
+}
+
+bool Queue::receive(void *data, TickType_t waitToReceive)
+{
+    return (xQueueReceive(m_hQueue, data, waitToReceive) == pdTRUE);
+}
+
+bool Queue::copy(void *data)
+{
+    return (xQueuePeek(m_hQueue, data, m_waitToReceive) == pdTRUE);
+}
+
+bool Queue::copy(void *data, TickType_t waitToCopy)
+{
+    return (xQueuePeek(m_hQueue, data, waitToCopy) == pdTRUE);
+}
+
+TickType_t Queue::waitingToSendTime() const
+{
+    return m_waitToSend;
+}
+
+TickType_t Queue::waitingToReceiveTime() const
+{
+    return m_waitToReceive;
+}
+
+UBaseType_t Queue::length() const
+{
+    return m_length;
+}
+
+UBaseType_t Queue::unitSize() const
+{
+    return m_unitSize;
+}
---
+++ Queue.h
@@ -0,0 +1,45 @@
+#include <Arduino_FreeRTOS.h>
+#include <queue.h>
+
+#if (configSUPPORT_DYNAMIC_ALLOCATION == 0)
+    #error "Dynamic allocation is not configured."
+#else
+
+#ifndef QUEUE_WH
+#define QUEUE_WH
+
+#define DEFAULT_TICKS_TO_WAIT   (500/portTICK_PERIOD_MS)  //[ms]
+
+class Queue
+{
+private:        
+    QueueHandle_t m_hQueue;
+    TickType_t    m_waitToSend;
+    TickType_t    m_waitToReceive;
+    UBaseType_t   m_length;
+    UBaseType_t   m_unitSize;
+
+public:
+    Queue(UBaseType_t length, UBaseType_t unitSize);
+    Queue(UBaseType_t length, UBaseType_t unitSize, TickType_t defaultSendReceiveTime);
+    Queue(UBaseType_t length, UBaseType_t unitSize, TickType_t waitToSend, TickType_t waitToReceive);
+
+
+    bool send(const void *data);
+    bool send(const void *data, TickType_t waitToSend);
+
+    bool receive(void *data);
+    bool receive(void *data, TickType_t waitToReceive);
+
+    bool copy(void *data);
+    bool copy(void *data, TickType_t waitToCopy);
+
+    TickType_t waitingToSendTime() const;
+    TickType_t waitingToReceiveTime() const;
+
+    UBaseType_t length() const;
+    UBaseType_t unitSize() const;
+};
+
+#endif
+#endif
