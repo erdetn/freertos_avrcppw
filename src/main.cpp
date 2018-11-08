52f57823eefa870cb472df690f83a2538be9b7cb
Erdet Nasufi <erdet.nasufi@gmail.com>
2018-11-08 21:08:46 +0100

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,34 +12,32 @@ Task *task2;
 
 static void ledTask()
 {
-	digitalWrite(ledPin, HIGH);
-	TaskDelay(500);
-	digitalWrite(ledPin, LOW);
-	TaskDelay(500);
+    digitalWrite(ledPin, HIGH);
+    TaskDelay(500);
+    digitalWrite(ledPin, LOW);
+    TaskDelay(500);
 }
 
 static void redLedTask()
 {
-	digitalWrite(redLedPin, HIGH);
-	TaskDelay(500);
-	digitalWrite(redLedPin, LOW);
-	TaskDelay(500);
+    digitalWrite(redLedPin, HIGH);
+    TaskDelay(500);
+    digitalWrite(redLedPin, LOW);
+    TaskDelay(500);
 }
-
 
 void setup()
 {
-	pinMode(ledPin, OUTPUT);
-	pinMode(redLedPin, OUTPUT);
+    pinMode(ledPin, OUTPUT);
+    pinMode(redLedPin, OUTPUT);
 
     task1 = new Task(ledTask, "task1", TaskPriority::MediumPriority, 100);
-	task2 = new Task(redLedTask, "task2", TaskPriority::LowPriority, 100);
+    task2 = new Task(redLedTask, "task2", TaskPriority::LowPriority, 100);
 
-	task1->run(NULL);
-	task2->run(NULL);
+    task1->run(NULL);
+    task2->run(NULL);
 }
 
 void loop()
 {
-    
 }
