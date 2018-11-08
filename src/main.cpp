e95c31caabf204b3e24784ced72f01528ae674ae
Erdet Nasufi <erdet.nasufi@gmail.com>
2018-11-08 21:08:07 +0100

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@
 using namespace urtos;
 
 const int ledPin = 13;
+const int redLedPin = 12;
+
 Task *task1;
+Task *task2;
 
 static void ledTask()
 {
@@ -15,10 +18,25 @@ static void ledTask()
 	TaskDelay(500);
 }
 
+static void redLedTask()
+{
+	digitalWrite(redLedPin, HIGH);
+	TaskDelay(500);
+	digitalWrite(redLedPin, LOW);
+	TaskDelay(500);
+}
+
+
 void setup()
 {
 	pinMode(ledPin, OUTPUT);
+	pinMode(redLedPin, OUTPUT);
+
     task1 = new Task(ledTask, "task1", TaskPriority::MediumPriority, 100);
+	task2 = new Task(redLedTask, "task2", TaskPriority::LowPriority, 100);
+
+	task1->run(NULL);
+	task2->run(NULL);
 }
 
 void loop()
